0c9b983a10c3cc714b9d108bb0fe832d37acd7a7
billTas <billtas@gmail.com>
2023-11-14 23:10:24 +0200

    Python lessons v2

--- 07.loops.py
+++ 07.loops.py
@@ -5,3 +5,14 @@ for i in [0, 1, 2 ,3 ,4, 5]:
 print("range")
 for i in range(6):
     print(i) 
+
+names = ["Harry", "Ron", "Hermione"]
+
+#prin th elist items
+for name in names:
+    print(name)
+
+#print the string characters
+name = "Harry"
+for chars in name:
+    print(chars)                                   
