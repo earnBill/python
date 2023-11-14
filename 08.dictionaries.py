01e6a09fdef848537939cc6ba824d4047b292ca1
billTas <billtas@gmail.com>
2023-11-14 23:10:47 +0200

    Python lessons v2

---
+++ 08.dictionaries.py
@@ -0,0 +1,9 @@
+houses = {"Harry": "Gryffindor", "Draco":"Slytherin"}
+
+print(houses["Harry"])
+print(f'Draco house is {houses["Draco"]}' )
+
+#add a new value to dictionary
+houses["Hermione"] = "Gryffindor"
+
+print(houses["Hermione"])
